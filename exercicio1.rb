60a566421cfd626608b2b8a81691ba82c1720065
diniscoelho <dinisc@outlook.pt>
2020-06-06 19:12:45 +0100

    semana 1

---
+++ exercicio1.rb
@@ -0,0 +1,16 @@
+#ENTRADA E SAIDA DE DADOS
+
+print 'Insira o seu nome: '
+
+
+nome=gets.chomp
+
+print 'Insira o seu sobrenome: '
+
+
+sobrenome=gets.chomp
+
+#escrever usando a informação que demos
+
+
+puts "Ola #{nome} #{sobrenome}"
---
+++ exercicio2.rb
@@ -0,0 +1,14 @@
+print 'Introduza um numero inteiro: '
+
+no1=gets.chomp.to_i
+
+
+print 'Introduza um numero inteiro: '
+
+no2=gets.chomp.to_i
+
+
+addition=no1+no2
+
+
+puts "A soma do numero 1 com o numero 2 e #{addition}"
